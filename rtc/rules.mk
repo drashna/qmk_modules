dd968fdbd00fa5cd1637a75d812c3f98a0323e23
Drashna Jael're <drashna@live.com>
2025-03-08 02:51:53 -0800

    Fix driver path for rtc rules

--- rtc/rules.mk
+++ rtc/rules.mk
@@ -5,7 +5,7 @@ ifeq ($(filter $(RTC_DRIVER),$(VALID_RTC_DRIVER_TYPES)),)
     $(call CATASTROPHIC_ERROR,Invalid RTC_DRIVER,RTC_DRIVER="$(RTC_DRIVER)" is not a valid RTC type)
 else
     OPT_DEFS += -D$(strip $(shell echo $(RTC_DRIVER) | tr '[:lower:]' '[:upper:]'))_RTC_DRIVER_ENABLE
-    SRC += modules/drashna/rtc/drivers/$(strip $(RTC_DRIVER)).c
+    SRC += drivers/$(strip $(RTC_DRIVER)).c
     ifeq ($(strip $(RTC_FORCE_INIT)), yes)
         OPT_DEFS += -DRTC_FORCE_INIT
     endif
