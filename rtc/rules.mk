5ed3363268295b54697bd56848a64ab44f9a5ee2
Drashna Jael're <drashna@live.com>
2025-04-25 14:12:17 -0700

    [RTC] Separate vendor driver into platform specific files

--- rtc/rules.mk
+++ rtc/rules.mk
@@ -5,13 +5,14 @@ ifeq ($(filter $(RTC_DRIVER),$(VALID_RTC_DRIVER_TYPES)),)
     $(call CATASTROPHIC_ERROR,Invalid RTC_DRIVER,RTC_DRIVER="$(RTC_DRIVER)" is not a valid RTC type)
 else
     OPT_DEFS += -D$(strip $(shell echo $(RTC_DRIVER) | tr '[:lower:]' '[:upper:]'))_RTC_DRIVER_ENABLE
-    SRC += drivers/$(strip $(RTC_DRIVER)).c
     ifeq ($(strip $(RTC_FORCE_INIT)), yes)
         OPT_DEFS += -DRTC_FORCE_INIT
     endif
     ifeq ($(strip $(RTC_DRIVER)), vendor)
         OPT_DEFS += -DHAL_USE_RTC=TRUE
+        SRC += drivers/$(strip $(RTC_DRIVER))_$(PLATFORM_KEY).c
     else
+        SRC += drivers/$(strip $(RTC_DRIVER)).c
         I2C_DRIVER_REQUIRED = yes
     endif
 endif
