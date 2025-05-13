8c2aed8c3d770548ad656e37723fe979c58c2420
Drashna Jael're <drashna@live.com>
2025-05-13 01:30:02 -0700

    [QP Helpers] Add multi array support to graphing

--- qp_helpers/qp_helpers.c
+++ qp_helpers/qp_helpers.c
@@ -1,5 +1,5 @@
 // Copyright 2025 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifdef QUANTUM_PAINTER_ENABLE
 #    include "qp_helpers.h"
@@ -9,7 +9,7 @@ static inline uint8_t scale_value(uint8_t value, uint8_t from, uint8_t to) {
 }
 
 bool qp_draw_graph(painter_device_t device, uint16_t graph_x, uint16_t graph_y, uint16_t graph_width,
-                   uint16_t graph_height, hsv_t primary, hsv_t secondary, hsv_t background, uint8_t* graph_data,
+                   uint16_t graph_height, hsv_t primary, hsv_t background, graph_line_t *graph_data, uint8_t n_graphs,
                    uint8_t graph_segments, uint8_t scale_to) {
     uint8_t graph_starting_index = 0;
     // if there are more segments than the graph width is wide in pixels, then set up things to only render the last
@@ -38,14 +38,19 @@ bool qp_draw_graph(painter_device_t device, uint16_t graph_x, uint16_t graph_y, 
 
     // Plot graph data
     uint8_t offset = 0;
-    for (uint8_t i = graph_starting_index; i < graph_segments; i++) {
-        offset += (remainder != 0 && (i - graph_starting_index) % (graph_segments / remainder) == 0) ? 1 : 0;
-        uint16_t x1 = graph_x + ((i - graph_starting_index) * spacing) + offset;
-        uint16_t y1 = graph_y + graph_height - scale_value(graph_data[i], graph_height - 1, scale_to) - 1;
-        uint16_t x2 = graph_x + (((i - graph_starting_index) + 1) * spacing) + offset;
-        uint16_t y2 = graph_y + graph_height - scale_value(graph_data[i + 1], graph_height - 1, scale_to) - 1;
-        if (!qp_line(device, x1, y1, x2, y2, secondary.h, secondary.s, secondary.v)) {
-            return false;
+    for (uint8_t n = 0; n < n_graphs; n++) {
+        for (uint8_t i = graph_starting_index; i < graph_segments; i++) {
+            offset += (remainder != 0 && (i - graph_starting_index) % (graph_segments / remainder) == 0) ? 1 : 0;
+            uint16_t x1 = graph_x + ((i - graph_starting_index) * spacing) + offset;
+            uint16_t y1 =
+                graph_y + graph_height - scale_value(graph_data[n].line_data[i], graph_height - 1, scale_to) - 1;
+            uint16_t x2 = graph_x + (((i - graph_starting_index) + 1) * spacing) + offset;
+            uint16_t y2 =
+                graph_y + graph_height - scale_value(graph_data[n].line_data[i + 1], graph_height - 1, scale_to) - 1;
+            if (!qp_line(device, x1, y1, x2, y2, graph_data[n].line_color.h, graph_data[n].line_color.s,
+                         graph_data[n].line_color.v)) {
+                return false;
+            }
         }
     }
 
--- qp_helpers/qp_helpers.h
+++ qp_helpers/qp_helpers.h
@@ -1,11 +1,17 @@
 // Copyright 2025 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
 
 #ifdef QUANTUM_PAINTER_ENABLE
 #    include <qp.h>
 #    include <stdlib.h>
+#    include <quantum/color.h>
+
+typedef struct {
+    uint8_t *line_data;
+    hsv_t    line_color;
+} graph_line_t;
 
 /**
  * @brief Renders a simple graph based on uint8_t[] array.
@@ -16,16 +22,15 @@
  * @param graph_width width of the graph in pixels
  * @param graph_height height of the graph in pixels
  * @param primary primary color for the graph (used for axis color)
- * @param secondary secondary color for the graph (used for the graph line color)
  * @param background background color for the graph (used for clearing the graph area)
- * @param graph_data array of uint8_t data to plot
+ * @param graph_data graph_line_t array containing the data to be plotted and color for each line
  * @param graph_segments number of segments in the graph_data array
  * @param scale_to maximum value to scale the graph to
  * @return true if the graph was drawn successfully
  * @return false if the graph could not be drawn
  */
 bool qp_draw_graph(painter_device_t device, uint16_t graph_x, uint16_t graph_y, uint16_t graph_width,
-                   uint16_t graph_height, hsv_t primary, hsv_t secondary, hsv_t background, uint8_t* graph_data,
+                   uint16_t graph_height, hsv_t primary, hsv_t background, graph_line_t *graph_data, uint8_t n_graphs,
                    uint8_t graph_segments, uint8_t scale_to);
 
 #endif
