b1158cb0c35ed257829603f6af8cf9442de58baf
Drashna Jaelre <drashna@drashna.net>
2026-03-20 12:38:18 -0700

    Update copyright header for dnd key

--- dnd_key/dnd_key.c
+++ dnd_key/dnd_key.c
@@ -1,5 +1,7 @@
 // Copyright 2025 Nick Brassel (@tzarc)
+// Copyright 2026 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
+
 #include QMK_KEYBOARD_H
 
 bool process_record_dnd_key(uint16_t keycode, keyrecord_t *record) {
