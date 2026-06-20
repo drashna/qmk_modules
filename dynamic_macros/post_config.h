2bf41eb9c8163cc7fa2f2b7dc983e4f903d499a7
Drashna Jael're <drashna@drashna.net>
2026-06-19 17:32:07 -0700

    Add Custom Dynamic Macros module with eeprom backing

---
+++ dynamic_macros/post_config.h
@@ -0,0 +1,8 @@
+// Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#ifndef EECONFIG_MODULE_DYNAMIC_MACROS_DATA_SIZE
+#    define EECONFIG_MODULE_DYNAMIC_MACROS_DATA_SIZE 6144
+#endif
