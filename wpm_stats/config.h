e8676a10a27c4859131a24e59bf4d35d12cc27b8
Drashna Jael're <drashna@drashna.net>
2026-07-30 16:53:22 -0700

    [WPM Stats] Fix split sync functionality

---
+++ wpm_stats/config.h
@@ -0,0 +1,6 @@
+// Copyright 2023 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#define SPLIT_TRANSACTION_IDS_MODULE_WPM_STATS RPC_ID_WPM_STATS_SYNC
--- wpm_stats/wpm_stats.c
+++ wpm_stats/wpm_stats.c
@@ -4,14 +4,57 @@
 
 #include "wpm_stats.h"
 #include <quantum.h>
+#if defined(SPLIT_KEYBOARD)
+#    include "transactions.h"
+#    ifndef FORCED_SYNC_THROTTLE_MS
+#        define FORCED_SYNC_THROTTLE_MS 1000
+#    endif
+#endif
 
+#ifdef SPLIT_KEYBOARD
+ASSERT_COMMUNITY_MODULES_MIN_API_VERSION(1, 1, 3);
+#else
 ASSERT_COMMUNITY_MODULES_MIN_API_VERSION(1, 0, 0);
+#endif
 
 static bool     g_initialized = false;
 static uint16_t g_last_update;
 static uint16_t g_max_wpm   = 0;
 static uint32_t g_wpm_sum   = 0;
 static uint16_t g_wpm_count = 0;
+
+#ifdef SPLIT_KEYBOARD
+
+/**
+ * @brief Structure to hold WPM statistics for synchronization between halves of a split keyboard
+ *
+ */
+typedef struct wpm_stat_config_t {
+    uint16_t max_wpm;
+    uint32_t wpm_sum;
+    uint16_t wpm_count;
+} wpm_stat_config_t;
+
+/**
+ * @brief Handler for receiving WPM statistics synchronization from the other half of a split keyboard
+ * @param initiator2target_buffer_size Size of the buffer sent from the initiator to the target
+ * @param initiator2target_buffer Buffer containing the WPM statistics sent from the initiator
+ * @param target2initiator_buffer_size Size of the buffer sent from the target to the initiator (not used in this
+ * handler)
+ * @param target2initiator_buffer Buffer to send
+ */
+static void wpm_state_sync_handler(uint8_t initiator2target_buffer_size, const void *initiator2target_buffer,
+                                   uint8_t target2initiator_buffer_size, void *target2initiator_buffer) {
+    static wpm_stat_config_t wpm_stat_config;
+
+    if (memcmp(initiator2target_buffer, &wpm_stat_config, sizeof(wpm_stat_config_t)) != 0) {
+        memcpy(&wpm_stat_config, initiator2target_buffer, sizeof(wpm_stat_config_t));
+        g_max_wpm   = wpm_stat_config.max_wpm;
+        g_wpm_sum   = wpm_stat_config.wpm_sum;
+        g_wpm_count = wpm_stat_config.wpm_count;
+    }
+}
+#endif // SPLIT_KEYBOARD
 
 void wpm_stats_init(void) {
     g_initialized = true;
@@ -24,11 +67,15 @@ void wpm_stats_init(void) {
 void keyboard_pre_init_wpm_stats(void) {
     wpm_stats_init();
 
+#ifdef SPLIT_KEYBOARD
+    transaction_register_rpc(RPC_ID_WPM_STATS_SYNC, wpm_state_sync_handler);
+#endif // SPLIT_KEYBOARD
+
     keyboard_pre_init_wpm_stats_kb();
 }
 
 void housekeeping_task_wpm_stats(void) {
-    if (!g_initialized) goto done;
+    if (!g_initialized || !is_keyboard_master()) goto done;
 
     if (timer_elapsed(g_last_update) >= 1000) { // Update every second
         uint16_t current_wpm = wpm_stats_get_current();
@@ -53,6 +100,32 @@ void housekeeping_task_wpm_stats(void) {
 
         g_last_update = timer_read();
     }
+
+#ifdef SPLIT_KEYBOARD
+    if (is_keyboard_master()) {
+        // Sync WPM stats to the other half of the keyboard
+        bool                     needs_sync           = false;
+        static uint16_t          last_sync            = 0;
+        static wpm_stat_config_t last_wpm_stat_config = {0};
+        wpm_stat_config_t        wpm_stat_config      = {
+            .max_wpm   = g_max_wpm,
+            .wpm_sum   = g_wpm_sum,
+            .wpm_count = g_wpm_count,
+        };
+
+        if (memcmp(&wpm_stat_config, &last_wpm_stat_config, sizeof(wpm_stat_config_t)) != 0) {
+            memcpy(&last_wpm_stat_config, &wpm_stat_config, sizeof(wpm_stat_config_t));
+            needs_sync = true;
+        }
+
+        if (timer_elapsed(last_sync) > FORCED_SYNC_THROTTLE_MS || needs_sync) {
+            if (transaction_rpc_send(RPC_ID_WPM_STATS_SYNC, sizeof(wpm_stat_config_t), &wpm_stat_config)) {
+                last_sync = timer_read();
+            }
+        }
+    }
+#endif // SPLIT_KEYBOARD
+
 done:
     housekeeping_task_wpm_stats_kb();
 }
@@ -70,43 +143,19 @@ bool wpm_stats_get(wpm_stats_t *stats) {
 }
 
 uint16_t wpm_stats_get_current(void) {
-    if (!g_initialized) return 0;
+    if (!g_initialized) return UINT16_MAX;
 
     return get_current_wpm();
 }
 
 uint16_t wpm_stats_get_avg(void) {
-    if (!g_initialized) return 0;
+    if (!g_initialized) return UINT16_MAX;
 
     return (g_wpm_count > 0) ? (uint16_t)(g_wpm_sum / g_wpm_count) : 0;
 }
 
 uint16_t wpm_stats_get_max(void) {
-    if (!g_initialized) return 0;
+    if (!g_initialized) return UINT16_MAX;
 
     return g_max_wpm;
 }
-
-#ifdef SPLIT_KEYBOARD
-bool wpm_stats_get_split(uint16_t *max_wpm, uint32_t *wpm_sum, uint16_t *wpm_count) {
-    if (!g_initialized) {
-        return false;
-    }
-    g_max_wpm   = *max_wpm;
-    g_wpm_sum   = *wpm_sum;
-    g_wpm_count = *wpm_count;
-
-    return true;
-}
-
-bool wpm_stats_set_split(uint16_t *max_wpm, uint32_t *wpm_sum, uint16_t *wpm_count) {
-    if (!g_initialized) {
-        return false;
-    }
-    *max_wpm   = g_max_wpm;
-    *wpm_sum   = g_wpm_sum;
-    *wpm_count = g_wpm_count;
-
-    return true;
-}
-#endif // SPLIT_KEYBOARD
